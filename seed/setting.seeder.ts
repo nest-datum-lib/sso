aef4c59ca0af6591cdb2b206f7655e5603b74f8a
ihor <ihor.bielchenko@gmail.com>
2022-11-29 09:58:39 +0100

    Update dependencies.

--- seed/setting.seeder.ts
+++ seed/setting.seeder.ts
@@ -78,9 +78,9 @@ export class SettingSeeder {
 				value: process.env.REDIS_CACHE_DB,
 				isNotDelete: true,
 			}, {
-				id: 'sso-setting-cache-db',
-				name: 'Cache db',
-				description: 'Redis database number.',
+				id: 'sso-setting-cache-password',
+				name: 'Cache password',
+				description: 'Redis database password.',
 				dataTypeId: 'data-type-type-text',
 				value: process.env.REDIS_CACHE_PASSWORD,
 				isNotDelete: true,
